c162afb2d89adfa88027ccd7e0a48b80eec81b7f
iamgabrielsoft <iamgabrielsoft@gmail.com>
2021-09-05 08:43:02 +0100

    frontend

---
+++ client/build/precache-manifest.036d2c72c9c27eb67e1072592fe73b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6a21d5dcd92a1482fc57d1f6fa8bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23e24eadb3285ff306a",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "815461470b4985145723",
+    "url": "/static/js/2.ed9c1616.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ed9c1616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23e24eadb3285ff306a",
+    "url": "/static/js/main.6796a4ca.chunk.js"
+  },
+  {
+    "revision": "f3851c033c97973a0da1",
+    "url": "/static/js/runtime-main.36cb09b9.js"
+  }
+]);
