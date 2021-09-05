fbabfbde6406a2bf233e224685837e19781902d0
iamgabrielsoft <iamgabrielsoft@gmail.com>
2021-09-05 08:44:20 +0100

    Backend

--- server.js
+++ server.js
@@ -35,6 +35,4 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold));
-
